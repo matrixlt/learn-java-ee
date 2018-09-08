f2c23c5a9eaafe7c2d3e49bf7c9b38a3070d72af
Lin Yinfeng <lin.yinfeng@outlook.com>
2018-09-08 12:01:54 +0800

    Suppress warning for Counter bean

--- servlet/servlet-playground/src/main/java/servletplayground/Counter.java
+++ servlet/servlet-playground/src/main/java/servletplayground/Counter.java
@@ -3,11 +3,12 @@ package servletplayground;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
+@SuppressWarnings({"unused", "WeakerAccess"})
 @Named
 @ApplicationScoped
 public class Counter {
     private Integer counter;
-    
+
     synchronized public Integer getInteger() {
         return this.counter;
     }
@@ -15,16 +16,19 @@ public class Counter {
     public Counter() {
         this.counter = 0;
     }
-    
+
     public Counter(Integer c) {
         this.counter = c;
     }
+
     public synchronized void setInteger(Integer c) {
         this.counter = c;
     }
+
     public synchronized void increase() {
         this.counter += 1;
     }
+
     public synchronized void decrease() {
         this.counter -= 1;
     }
